a5c5a82dfe0c281c85abb8cd7943e32621b9b6b7
Stephan Jennewein <stephan@selfnet.de>
2013-12-07 21:09:46 +0100

    removed unnecessary if statement

--- functions/Igor/Mat2Igor.m
+++ functions/Igor/Mat2Igor.m
@@ -1,6 +1,7 @@
 function [ igor ] = Mat2Igor( image )
 %Mat2Igor Translates 2D image matrix to Igor compatible format
 %   hands back an array that has x-index,y-index,value
+	tic
     vectorizedImage = reshape(image,1,[]);
     [length ~] = size(vectorizedImage);
     [x y] = size(image);
@@ -9,15 +10,12 @@ function [ igor ] = Mat2Igor( image )
     igor = zeros(length,3);    
     for i=1:x
         start = (i-1) * 400 + 1;
-        
-        if(start == 0)
-            start = 1;
-        end
-        
+                       
         stop = i*400;
         igor(start:stop,1) = xAxis';
         igor(start:stop,2) = yAxis' * i;
     end
     igor(:,3) = vectorizedImage;
+	toc
 end
 
