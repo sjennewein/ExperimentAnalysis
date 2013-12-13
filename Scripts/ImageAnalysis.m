2d0765682885d37c7a60e2ace816008919633623
Stephan Jennewein <stephan@selfnet.de>
2013-12-13 18:46:33 +0100

    worked on background fitting

--- Scripts/ImageAnalysis.m
+++ Scripts/ImageAnalysis.m
@@ -1,10 +1,12 @@
 clear all;
 
-addpath('C:\Users\stephan\Documents\FrenchyMatlab\functions\WinSpec\');
-addpath('C:\Users\stephan\Documents\FrenchyMatlab\functions\Visualize\');
+addpath('..\functions\WinSpec\');
+addpath('..\functions\fitting\');
+addpath('..\functions\Visualize\');
+addpath('..\functions\Igor\');
 
 header = speread_header('C:\Users\stephan\Documents\data\Atoms_Macro_tof_40µs_651µW_90seq.SPE');
 frame = speread_frame(header,1);
-roiX = [125 300];
-roiY = [125 300];
-ShowROI(frame, roiX, roiY);
+backgroundROI = ones(400,400);
+backgroundROI(126:299,126:299) = NaN(174,174);
+
--- functions/fitting/Polynom2XY.m
+++ functions/fitting/Polynom2XY.m
@@ -1,7 +1,10 @@
-function [ F ] = Polynom2XY( x,y,p )
+function z = Polynom2XY( a0, a1, a2, b1, b2, x, y )
 %POLYNOM2XY Summary of this function goes here
 %   Detailed explanation goes here
+    X = x;
+    Y = y;
 
+    z = a0 + a1.*X + b1.*Y + a2.*X.^2 + b2.*Y.^2 + 2 .* a2 .* b2 .* X .* Y;
 
 end
 
