aa2286edea4e696dbf7ad548bcc51e18be02c583
Stephan Jennewein <stephan@selfnet.de>
2014-02-25 11:48:36 +0100

    added script that can extract parameters from filename

---
+++ functions/parameterFromFilename.m
@@ -0,0 +1,23 @@
+function  [names, values, units]  = parameterFromFilename( filename )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    
+    input = lower(filename);
+    [~,name,~] = fileparts(input);
+    
+    delimited = strsplit(name,'_');
+    headerLength = numel(delimited{1}) + 1;
+    parameterList = name(headerLength:end);
+    parameters = regexpi(parameterList, '(_\w+?_\d+?\.?\d+?[a-zµ]+)','tokens');
+    names = cell(1,numel(parameters));
+    values = zeros(1,numel(parameters));
+    units = cell(1,numel(parameters));
+    
+    for i = 1:numel(parameters)
+        match = regexpi(parameters{i},'_(\w+?)_(\d+?\.?\d+?)([a-zµ]+)','tokens');
+        names{i} = match{1}{1}{1};
+        values(i) = str2double(match{1}{1}{2});
+        units{i} = match{1}{1}{3};
+    end
+end
+
