093f8f02b2264a9e94a67f731cc441b6d63f322d
Stephan Jennewein <stephan@selfnet.de>
2014-02-27 11:58:07 +0100

    fixed ShowRoi to work on a cell array of ImageResult

--- functions/Visualize/ShowROI.m
+++ functions/Visualize/ShowROI.m
@@ -1,19 +1,45 @@
-function [ ] = ShowROI( picture, ROI )
-%SHOWROI Shows the ccd picture and draws ROI
-%   Shows the renormalized picture and draws the ROI
-          
-    imagesc(flipud( picture ));
-    hold on;
+function ShowROI( data )
+%SHOWROI Shows the flattened picture and draws ROI
+%   Shows the flattened picture and draws the ROI
+      
+    if(~iscell(data))
+        error('Data must be a cell array!');
+    end
     
-     plot(ROI(1,1),ROI(1,2):ROI(2,2),'linewidth',1.5,'Color','black'); %plot lower x boundary
-     plot(ROI(2,1),ROI(1,2):ROI(2,2),'linewidth',1.5,'Color','black'); %plot upper x boundary
+    for iData = 1:numel(data)
+        if(~isa(data{iData},'ImageResult'))
+            error('Only data of type ImageResult can be processed');
+        end            
+    end
     
-     plot(ROI(1,1):ROI(2,1),ROI(1,2),'linewidth',1.5,'Color','black');
-     plot(ROI(1,1):ROI(2,1),ROI(2,2),'linewidth',1.5,'Color','black');
+    for iData = 1:numel(data)
+        figureTitle = '';        
+        for iParameter = 1:numel(data{iData}.pName)
+            figureTitle = strcat(figureTitle, ...
+                          data{iData}.pName{iParameter}, '_', ...
+                          num2str(data{iData}.pValue(iParameter)), ...
+                          data{iData}.pUnit{iParameter}, '_');
+        end
+        figure('name', figureTitle);
+        
+        imagesc(flipud( data{iData}.flat ));
+        colorbar;
+        hold on;
+        x1 = data{iData}.ROI(1,1);
+        x2 = data{iData}.ROI(2,1);
+        y1 = data{iData}.ROI(1,2);
+        y2 = data{iData}.ROI(2,2);
     
-    set(gca,'ydir','normal');
-    hold off;
     
+        plot(x1,y1:y2,'linewidth',1.5,'Color','black'); %plot lower x boundary
+        plot(x2,y1:y2,'linewidth',1.5,'Color','black'); %plot upper x boundary
+    
+        plot(x1:x2,y1,'linewidth',1.5,'Color','black');
+        plot(x1:x2,y2,'linewidth',1.5,'Color','black');
+    
+        set(gca,'ydir','normal');
+        hold off;
+    end
 
 end
 
