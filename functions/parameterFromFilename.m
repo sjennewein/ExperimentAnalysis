192c5ff005c2f4a214565f08aa4fa62078d7cb31
Lab Computer <stephan@institutoptique.fr>
2014-03-06 16:58:06 +0100

    fixed regex error to not match all parameters

--- functions/parameterFromFilename.m
+++ functions/parameterFromFilename.m
@@ -8,13 +8,14 @@ function  [names, values, units]  = parameterFromFilename( filename )
     delimited = strsplit(name,'_');
     headerLength = numel(delimited{1}) + 1;
     parameterList = name(headerLength:end);
-    parameters = regexpi(parameterList, '(_\w+?_\d+?\.?\d+?[a-zµ]+)','tokens');
+    parameters = regexpi(parameterList, '(_\w+?_\d+?(\.?\d+?)*[a-zµ]+)','tokens');
+    
     names = cell(1,numel(parameters));
     values = zeros(1,numel(parameters));
     units = cell(1,numel(parameters));
     
     for i = 1:numel(parameters)
-        match = regexpi(parameters{i},'_(\w+?)_(\d+?\.?\d+?)([a-zµ]+)','tokens');
+        match = regexpi(parameters{i},'_(\w+?)_(\d+?(\.?\d+?)*)([a-zµ]+)','tokens');
         names{i} = match{1}{1}{1};
         values(i) = str2double(match{1}{1}{2});
         units{i} = match{1}{1}{3};
