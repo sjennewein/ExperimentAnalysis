fe965f6fd4849a2784bdce87e8d2131fc3298494
Stephan Jennewein <stephan@selfnet.de>
2014-03-27 14:05:49 +0100

    mistake fixed visualizing the matrix. now the coordinates in the pictures are the indices of the matrix and not of by some weird offset

--- functions/Analyze/ShowROI.m
+++ functions/Analyze/ShowROI.m
@@ -22,20 +22,24 @@ function ShowROI( data )
         end
         figure('name', figureTitle);
         
-        imagesc(flipud( data{iData}.flat ));
+        imagesc(data{iData}.flat );
+        axis xy;
         colorbar;
         hold on;
+        %Attention we use the x and y values as we read it from the picture
+        %not in the weird way we have to do it in the analysis function.
+        %for more information have a look in the analysis script
         x1 = data{iData}.ROI(1,1);
         x2 = data{iData}.ROI(2,1);
         y1 = data{iData}.ROI(1,2);
         y2 = data{iData}.ROI(2,2);
     
     
-        plot(x1,y1:y2,'linewidth',1.5,'Color','black'); %plot lower x boundary
-        plot(x2,y1:y2,'linewidth',1.5,'Color','black'); %plot upper x boundary
+        plot(x1,y1:y2,'+-','linewidth',1.5,'Color','black'); %plot lower x boundary
+        plot(x2,y1:y2,'+-','linewidth',1.5,'Color','black'); %plot upper x boundary
     
-        plot(x1:x2,y1,'linewidth',1.5,'Color','black');
-        plot(x1:x2,y2,'linewidth',1.5,'Color','black');
+        plot(x1:x2,y1,'+-','linewidth',1.5,'Color','black');
+        plot(x1:x2,y2,'+-','linewidth',1.5,'Color','black');
     
         set(gca,'ydir','normal');
         hold off;
