88cb00083992d08ec0940774714899b1971bdff3
Stephan Jennewein <stephan@selfnet.de>
2014-08-25 15:55:32 +0200

    fixed EncircledEnergy

--- functions/Analyze/EncircledEnergy.m
+++ functions/Analyze/EncircledEnergy.m
@@ -8,7 +8,7 @@ end
 [dimY, dimX] = size(data{1}.flat);
 [rr, cc] = meshgrid(1:dimX,1:dimY);
 
-sigma = [0.682 0.954 0.997 0.999936 0.999999 0.999999998 0.999999999997];
+sigma = [0.1 0.5 1 1.5 2 2.5 3 3.5 4 5 6 7 8 9 10];
 
 for iData = 1:numel(data)
     encircled = zeros(1,numel(sigma));
@@ -20,17 +20,20 @@ for iData = 1:numel(data)
     x0 = round(coeff(x0Index));
     y0 = round(coeff(y0Index));
 
-    for iSigma = 1:numel(sigma)
-        errorbar = confint(data{iData}.cloudFit,sigma(iSigma));
-        xWidth = errorbar(1,xWidthIndex);
-        yWidth = errorbar(1,yWidthIndex);
-        radius = mean([xWidth yWidth]);
-        mask = sqrt((rr-x0).^2+(cc-y0).^2)<=radius;
+    for iSigma = 1:numel(sigma)        
+        xWidth = coeff(xWidthIndex);
+        yWidth = coeff(yWidthIndex);
+        radius = round(mean([xWidth yWidth])/2);
+        mask = ((rr-x0).^2+(cc-y0).^2)<= (sigma(iSigma)*radius)^2;    
+        figure;
+        imagesc(mask);
         encircled(iSigma) = sum(sum(data{iData}.flat .* mask));
     end
     figure;
-    plot(encircled,'x-');
+    plot(sigma, encircled,'x-');
     hold on;
+    xlabel('n x width');
+    ylabel('Encircled Energy');
     limits = ylim;
     if(limits(1) < 0)
         ylim([limits(1) - 10 0]);
@@ -41,4 +44,3 @@ for iData = 1:numel(data)
     
 end
 end
-
